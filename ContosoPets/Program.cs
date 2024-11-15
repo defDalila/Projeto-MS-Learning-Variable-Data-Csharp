1c510866e7cae8b9b53f96b47b6c095b99d42777
defDalila <defdalila@outlook.com>
2024-11-15 17:53:20 -0300

    feat(search): implement dog characteristic search functionality

--- ContosoPets/Program.cs
+++ ContosoPets/Program.cs
@@ -72,6 +72,7 @@ for (int i = 0; i < maxPets; i++)
 			animalNickname = "Lion";
 			suggestedDonation = "";
 			break;
+			
 
 		default:
 			animalSpecies = "";
@@ -84,7 +85,7 @@ for (int i = 0; i < maxPets; i++)
 			break;
 
 	}
-	
+
 
 	ourAnimals[i, 0] = animalID;
 	ourAnimals[i, 1] = animalSpecies;
@@ -92,13 +93,13 @@ for (int i = 0; i < maxPets; i++)
 	ourAnimals[i, 3] = animalAge;
 	ourAnimals[i, 4] = animalPhysicalDescription;
 	ourAnimals[i, 5] = animalPersonalityDescription;
-	
-	
+
+
 	if (!decimal.TryParse(suggestedDonation, out petDonation))
 	{
 		petDonation = 45.00m;
 	}
-	
+
 	ourAnimals[i, 6] = $"{petDonation:C2}";
 
 }
@@ -146,7 +147,46 @@ do
 
 		case "2":
 			// Display all dogs with a specified characteristic
-			Console.WriteLine("\nUNDER CONSTRUCTION - please check back next month to see progress.");
+			string dogCharacteristic = "";
+
+			while (dogCharacteristic == "")
+			{
+				Console.Write("\nEnter one desired dog characteristics to search for: ");
+				readResult = Console.ReadLine();
+
+				if (readResult != null)
+				{
+					dogCharacteristic = readResult.ToLower().Trim();
+				}
+
+			}
+
+			string dogDescription;
+			bool noMatchesDog = true;
+
+			// #6 loop through the ourAnimals array to search for matching animals
+			for (int i = 0; i < maxPets; i++)
+			{
+				if (ourAnimals[i, 1].Contains("dog"))
+				{
+					// #7 Search combined descriptions and report results
+					dogDescription = ourAnimals[i, 4] + " " + ourAnimals[i, 5];
+
+					if (dogDescription.Contains(dogCharacteristic))
+					{
+						Console.WriteLine($"\nOur dog {ourAnimals[i, 3]} is a match!");
+						Console.WriteLine(dogDescription);
+
+						noMatchesDog = false;
+					}
+				}
+			}
+
+			if (noMatchesDog)
+			{
+				Console.WriteLine("None of our dogs are a match found for: " + dogCharacteristic);
+			}
+
 			Console.WriteLine("Press the Enter key to continue.");
 			readResult = Console.ReadLine();
 			break;
